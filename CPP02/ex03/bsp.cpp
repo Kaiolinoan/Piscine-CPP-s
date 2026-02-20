c9bfa951f69910d70f8dcff38f7f54908feda596
Kaio <kaiolinoan@gmail.com>
2026-02-20 16:59:56 +0000

    CPP02 bonus update

---
+++ CPP02/ex03/bsp.cpp
@@ -0,0 +1,24 @@
+#include "Point.hpp"
+
+static Fixed calculate_triangle_area(Fixed x1, Fixed y1, Fixed x2, Fixed y2, Fixed x3, Fixed y3)
+{
+    return ((x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))* Fixed(0.5f));
+}
+static Fixed absFixed(Fixed x)
+{
+    if (x < Fixed(0))
+        return x * Fixed(-1);
+    return x;
+}
+
+bool bsp( Point const a, Point const b, Point const c, Point const point)
+{
+    Fixed A_ABC = absFixed(calculate_triangle_area(a.getX(), a.getY(), b.getX(), b.getY(), c.getX(), c.getY()));
+    Fixed A_PAB = absFixed(calculate_triangle_area(point.getX(), point.getY(), a.getX(), a.getY(), b.getX(), b.getY()));
+    Fixed A_PBC = absFixed(calculate_triangle_area(point.getX(), point.getY(), b.getX(), b.getY(), c.getX(), c.getY()));
+    Fixed A_PAC = absFixed(calculate_triangle_area(point.getX(), point.getY(), a.getX(), a.getY(), c.getX(), c.getY()));
+    Fixed sum = A_PAB + A_PAC + A_PBC;
+    if (A_PAB == 0|| A_PAC == 0 || A_PBC == 0)
+        return (false);
+    return (absFixed(sum - A_ABC) < Fixed(0.01f));
+}
